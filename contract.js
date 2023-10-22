12fc37210c80743d6dd38345082555d8b01808d1
Chris Swenor <cswenor@gmail.com>
2023-10-22 07:51:36 -0400

    Added better error handling

--- contract.js
+++ contract.js
@@ -6,10 +6,10 @@ class CONTRACT {
         this.algodClient = algodClient;
         this.contractABI = new algosdk.ABIContract(spec);
         this.mnemonic = mnemonic;
-        
+
         for (const methodSpec of spec.methods) {
             const abiMethod = this.contractABI.getMethodByName(methodSpec.name);
-            this[methodSpec.name] = async function(...args) {
+            this[methodSpec.name] = async function (...args) {
                 if (methodSpec.readonly) {
                     return await this.createAndSimulateTxn(abiMethod, args);
                 } else {
@@ -24,66 +24,80 @@ class CONTRACT {
     async createAndSendTxn(abiMethod, args, simulationResult) {
         // logic to create and send a real transaction using simulationResult
     }
-    
-    async createAndSimulateTxn(abiMethod, args) {
-        // Get the suggested transaction parameters
-        const params = await this.algodClient.getTransactionParams().do();
-        
-        // Get the sender address from the mnemonic
-        const { addr: senderAddress, sk: privateKey } = algosdk.mnemonicToSecretKey(this.mnemonic);
 
-        // Encode arguments
-        const encodedArgs = args.map((arg, index) => {
-            return abiMethod.args[index].type.encode(arg);
-        });
-        
-        // Create the application call transaction object
-        const txn = algosdk.makeApplicationCallTxnFromObject({
-            suggestedParams: {
-                ...params,
-                flatFee: true,
-                fee: 1000
-            },
-            from: senderAddress,
-            appIndex: this.contractId,
-            appArgs: [abiMethod.getSelector(), ...encodedArgs],  // Adjust appArgs based on methodSpec and args
-        });
-        
-        // Sign the transaction
-        const signedTxn = algosdk.encodeUnsignedSimulateTransaction(txn);
-        
-        // Construct the simulation request
-        const request = new algosdk.modelsv2.SimulateRequest({
-            txnGroups: [
-                new algosdk.modelsv2.SimulateRequestTransactionGroup({
-                    txns: [algosdk.decodeObj(signedTxn)]
-                })
-            ],
-            allowUnnamedResources: true,
-            allowEmptySignatures: true
-        });
-        
-        // Simulate the transaction group
-        const response = await this.algodClient.simulateTransactions(request).do();
-        return this.handleSimulationResponse(response, abiMethod);
-    }
-    
-    handleSimulationResponse(response, abiMethod) {
-        // Handle the simulation results
-        const rlog = response.txnGroups[0].txnResults[0].txnResult.logs.pop();
-        const rlog_ui = Uint8Array.from(Buffer.from(rlog, 'base64'));
-        const res_ui = rlog_ui.slice(4);
-        
-        // Decode the response based on the methodSpec
-        //HACK: Hacking this because the decode function doesn't work on bytes
-        let result;
-        if (abiMethod.returns.type.childType == 'byte') {
-            result = new TextDecoder().decode(res_ui);
-        } else {
-            result = abiMethod.returns.type.decode(res_ui);
+    async createAndSimulateTxn(abiMethod, args) {
+        try {
+            // Get the suggested transaction parameters
+            const params = await this.algodClient.getTransactionParams().do();
+
+            // Get the sender address from the mnemonic
+            const { addr: senderAddress, sk: privateKey } = algosdk.mnemonicToSecretKey(this.mnemonic);
+
+            // Encode arguments
+            const encodedArgs = args.map((arg, index) => {
+                return abiMethod.args[index].type.encode(arg);
+            });
+
+            // Create the application call transaction object
+            const txn = algosdk.makeApplicationCallTxnFromObject({
+                suggestedParams: {
+                    ...params,
+                    flatFee: true,
+                    fee: 1000
+                },
+                from: senderAddress,
+                appIndex: this.contractId,
+                appArgs: [abiMethod.getSelector(), ...encodedArgs],  // Adjust appArgs based on methodSpec and args
+            });
+
+            // Sign the transaction
+            const signedTxn = algosdk.encodeUnsignedSimulateTransaction(txn);
+
+            // Construct the simulation request
+            const request = new algosdk.modelsv2.SimulateRequest({
+                txnGroups: [
+                    new algosdk.modelsv2.SimulateRequestTransactionGroup({
+                        txns: [algosdk.decodeObj(signedTxn)]
+                    })
+                ],
+                allowUnnamedResources: true,
+                allowEmptySignatures: true
+            });
+
+            // Simulate the transaction group
+            const response = await this.algodClient.simulateTransactions(request).do();
+            return this.handleSimulationResponse(response, abiMethod);
+        } catch (error) {
+            // console.error('Error in createAndSimulateTxn:', error);
+            throw error;  // Re-throw the error after logging it
         }
-        
-        return result;
+
+    }
+
+    handleSimulationResponse(response, abiMethod) {
+        try {
+            // Handle the simulation results
+            if (response.txnGroups[0].failureMessage) {
+                throw (response.txnGroups[0].failureMessage);
+            }
+            const rlog = response.txnGroups[0].txnResults[0].txnResult.logs.pop();
+            const rlog_ui = Uint8Array.from(Buffer.from(rlog, 'base64'));
+            const res_ui = rlog_ui.slice(4);
+
+            // Decode the response based on the methodSpec
+            //HACK: Hacking this because the decode function doesn't work on bytes
+            let result;
+            if (abiMethod.returns.type.childType == 'byte') {
+                result = new TextDecoder().decode(res_ui);
+            } else {
+                result = abiMethod.returns.type.decode(res_ui);
+            }
+
+            return result;
+        } catch (error) {
+            // console.error('Error in handleSimulationResponse:', error);
+            throw error;  // Re-throw the error after logging it
+        }
     }
 
 }
--- index.js
+++ index.js
@@ -19,39 +19,44 @@ const contractsData = [
 
 (async () => {
     for (const contractData of contractsData) {
-        const contractInstance = new CONTRACT(contractData.contractId, algodClient, ARC200Spec, process.env.WALLET_MNEMONIC);
-        console.log(`Processing ${contractData.contractInstanceName}:`);
+        try {
+            const contractInstance = new CONTRACT(contractData.contractId, algodClient, ARC200Spec, process.env.WALLET_MNEMONIC);
+            console.log(`Processing ${contractData.contractInstanceName}:`);
 
-        const name = await contractInstance.arc200_name();
-        console.log(`Name: ${name}`);
+            const name = await contractInstance.arc200_name();
+            console.log(`Name: ${name}`);
 
-        const symbol = await contractInstance.arc200_symbol();
-        console.log(`Symbol: ${symbol}`);
+            const symbol = await contractInstance.arc200_symbol();
+            console.log(`Symbol: ${symbol}`);
 
-        const totalSupply = await contractInstance.arc200_totalSupply();
-        console.log(`Total Supply: ${totalSupply}`);
+            const totalSupply = await contractInstance.arc200_totalSupply();
+            console.log(`Total Supply: ${totalSupply}`);
 
-        const decimals = await contractInstance.arc200_decimals();
-        console.log(`Number of Decimals: ${decimals}`);
+            const decimals = await contractInstance.arc200_decimals();
+            console.log(`Number of Decimals: ${decimals}`);
 
-        const wallet1 = 'C5NZ5SNL5EMOEVKFW3DS3DBG3FNMIYJAJY3U4I5SRCOXHGY33ML3TGHD24';
-        const wallet2 = 'OOEDQF6YL44JOIFBDXWVNREBXQ4IL53JMTA32R66S7GLKEP5WC4CL4SFLE';
-        const wallet3 = '3JOOMFDHWZ2YM2WD2HRPR6Q2DJ6KXH4WES3ZSYFVPZ3ORJQPNLL7LKNJRM';
+            const wallet1 = 'C5NZ5SNL5EMOEVKFW3DS3DBG3FNMIYJAJY3U4I5SRCOXHGY33ML3TGHD24';
+            const wallet2 = 'OOEDQF6YL44JOIFBDXWVNREBXQ4IL53JMTA32R66S7GLKEP5WC4CL4SFLE';
+            const wallet3 = '3JOOMFDHWZ2YM2WD2HRPR6Q2DJ6KXH4WES3ZSYFVPZ3ORJQPNLL7LKNJRM';
+            const wallet4 = 'BUD2763FMK6EYVKGHWWUN4QKHPSPCVFUEPPI4PQCPGYVPGQ6GNKBX6IXCQ';
 
-        const balance = await contractInstance.arc200_balanceOf(wallet1);
-        console.log(`Balance of ${wallet1}: ${balance}`);
+            const balance = await contractInstance.arc200_balanceOf(wallet1);
+            console.log(`Balance of ${wallet1}: ${balance}`);
 
-        const allowance = await contractInstance.arc200_allowance(wallet1, wallet2);
-        console.log(`Allowance from: ${wallet1} to: ${wallet2} total: ${allowance}`);
+            const allowance = await contractInstance.arc200_allowance(wallet1, wallet2);
+            console.log(`Allowance from: ${wallet1} to: ${wallet2} total: ${allowance}`);
 
-        const transfer = await contractInstance.arc200_transfer(wallet1, 1);
-        console.log(transfer);
+            const transfer = await contractInstance.arc200_transfer(wallet4, 1);
+            console.log(transfer);
 
-        const transferFrom = await contractInstance.arc200_transferFrom(wallet1, wallet2, 1);
-        console.log(transferFrom);
+            const transferFrom = await contractInstance.arc200_transferFrom(wallet1, wallet2, 1);
+            console.log(transferFrom);
 
-        //TODO: Errors Out
-        // const approve = await contractInstance.arc200_approve(wallet1, 0);
-        // console.log(approve);
+            //TODO: Errors Out
+            const approve = await contractInstance.arc200_approve(wallet1, 0);
+            console.log(approve);
+        } catch (error) {
+            console.error(`Error processing ${contractData.contractInstanceName}:`, error);
+        }
     }
 })();
