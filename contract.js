8973caee47a1f117621ce2a52db0008c5c07605b
Nicholas Shellabarger <temptemp3@users.noreply.github.com>
2023-10-22 22:40:33 -0600

    refactor, add safe methods

--- contract.js
+++ contract.js
@@ -1,120 +1,152 @@
-const algosdk = require('algosdk');
+const algosdk = require("algosdk");
 
 class CONTRACT {
-    constructor(contractId, algodClient, spec, mnemonic) {
-        this.contractId = contractId;
-        this.algodClient = algodClient;
-        this.contractABI = new algosdk.ABIContract(spec);
-        this.mnemonic = mnemonic;
-
-        for (const methodSpec of spec.methods) {
-            const abiMethod = this.contractABI.getMethodByName(methodSpec.name);
-            this[methodSpec.name] = async function (...args) {
-                if (methodSpec.readonly) {
-                    return await this.createAndSimulateTxn(abiMethod, args);
-                } else {
-                    return await this.createAndSimulateTxn(abiMethod, args);
-                    // const simulationResult = await this.createAndSimulateTxn(abiMethod, args);
-                    // return await this.createAndSendTxn(abiMethod, args, simulationResult);
-                }
-            }.bind(this);
+  constructor(contractId, algodClient, spec, mnemonic) {
+    this.contractId = contractId;
+    this.algodClient = algodClient;
+    this.contractABI = new algosdk.ABIContract(spec);
+    this.mnemonic = mnemonic;
+    this.simulate = true;
+    this.paymentAmount = 0;
+    this.simulationResultHandler = this.decodeSimulationResponse;
+    for (const methodSpec of spec.methods) {
+      const abiMethod = this.contractABI.getMethodByName(methodSpec.name);
+      this[methodSpec.name] = async function (...args) {
+        if (methodSpec.readonly || this.simulate) {
+          return await this.createAndSimulateTxn(abiMethod, args);
+        } else {
+          return await this.createAndSimulateTxn(abiMethod, args);
+          // const simulationResult = await this.createAndSimulateTxn(abiMethod, args);
+          // return await this.createAndSendTxn(abiMethod, args, simulationResult);
         }
+      }.bind(this);
     }
+  }
 
-    async createAndSendTxn(abiMethod, args, simulationResult) {
-        // logic to create and send a real transaction using simulationResult
+  getSenderAddress() {
+    const { addr: senderAddress } = algosdk.mnemonicToSecretKey(this.mnemonic);
+    return senderAddress;
+  }
+
+  setPaymentAmount(amount) {
+    this.paymentAmount = amount;
+  }
+
+  setSimulate(simulate) {
+    this.simulate = simulate;
+  }
+
+  async createAndSendTxn(abiMethod, args, simulationResult) {
+    // logic to create and send a real transaction using simulationResult
+  }
+
+  async createAndSimulateTxn(abiMethod, args) {
+    let response;
+    try {
+      response = await this.simulateTxn(abiMethod, args);
+      return this.handleSimulationResponse(response, abiMethod);
+    } catch (error) {
+      // console.error('Error in createAndSimulateTxn:', error);
+      //throw error; // Re-throw the error after logging it
+      return { success: false, response };
     }
+  }
 
-    async createAndSimulateTxn(abiMethod, args) {
-        try {
-            // Get the suggested transaction parameters
-            const params = await this.algodClient.getTransactionParams().do();
+  async simulateTxn(abiMethod, args) {
+    try {
+      // Get the suggested transaction parameters
+      const params = await this.algodClient.getTransactionParams().do();
 
-            // Get the sender address from the mnemonic
-            const { addr: senderAddress, sk: privateKey } = algosdk.mnemonicToSecretKey(this.mnemonic);
+      // Get the sender address from the mnemonic
+      const { addr: senderAddress, sk: privateKey } =
+        algosdk.mnemonicToSecretKey(this.mnemonic);
 
-            // Encode arguments
-            const encodedArgs = args.map((arg, index) => {
-                return abiMethod.args[index].type.encode(arg);
-            });
+      // Encode arguments
+      const encodedArgs = args.map((arg, index) => {
+        return abiMethod.args[index].type.encode(arg);
+      });
 
-            // Create the application call transaction object
-            const txn2 = algosdk.makeApplicationCallTxnFromObject({
-                suggestedParams: {
-                    ...params,
-                    flatFee: true,
-                    fee: 1000
-                },
-                from: senderAddress,
-                appIndex: this.contractId,
-                appArgs: [abiMethod.getSelector(), ...encodedArgs],  // Adjust appArgs based on methodSpec and args
-            });
-
-            const txn1 = algosdk.makePaymentTxnWithSuggestedParams(
-                senderAddress,
-                algosdk.getApplicationAddress(this.contractId),
-                100000,
-                undefined,
-                undefined,
-                {
-                    ...params,
-                    flatFee: true,
-                    fee: 1000
-                }
-              );
-
-            const txngroup = algosdk.assignGroupID([txn1,txn2]);
-            // Sign the transaction
-            const signedTxn = algosdk.encodeUnsignedSimulateTransaction(txngroup[0]);
-            const signedTxn2 = algosdk.encodeUnsignedSimulateTransaction(txngroup[1]);
-
-            // Construct the simulation request
-            const request = new algosdk.modelsv2.SimulateRequest({
-                txnGroups: [
-                    new algosdk.modelsv2.SimulateRequestTransactionGroup({
-                        txns: [algosdk.decodeObj(signedTxn), algosdk.decodeObj(signedTxn2)]
-                    })
-                ],
-                allowUnnamedResources: true,
-                allowEmptySignatures: true
-            });
-
-            // Simulate the transaction group
-            const response = await this.algodClient.simulateTransactions(request).do();
-            return this.handleSimulationResponse(response, abiMethod);
-        } catch (error) {
-            // console.error('Error in createAndSimulateTxn:', error);
-            throw error;  // Re-throw the error after logging it
+      // Create the application call transaction object
+      const txn2 = algosdk.makeApplicationCallTxnFromObject({
+        suggestedParams: {
+          ...params,
+          flatFee: true,
+          fee: 1000,
+        },
+        from: senderAddress,
+        appIndex: this.contractId,
+        appArgs: [abiMethod.getSelector(), ...encodedArgs], // Adjust appArgs based on methodSpec and args
+      });
+      const txn1 = algosdk.makePaymentTxnWithSuggestedParams(
+        senderAddress,
+        algosdk.getApplicationAddress(this.contractId),
+        this.paymentAmount,
+        undefined,
+        undefined,
+        {
+          ...params,
+          flatFee: true,
+          fee: 1000,
         }
+      );
 
+      const txns = [txn1, txn2];
+
+      const txngroup = algosdk.assignGroupID(txns);
+      // Sign the transaction
+      const stxns = txns.map(algosdk.encodeUnsignedSimulateTransaction);
+
+      // Construct the simulation request
+      const request = new algosdk.modelsv2.SimulateRequest({
+        txnGroups: [
+          new algosdk.modelsv2.SimulateRequestTransactionGroup({
+            txns: stxns.map(algosdk.decodeObj),
+          }),
+        ],
+        allowUnnamedResources: true,
+        allowEmptySignatures: true,
+      });
+
+      // Simulate the transaction group
+      const response = await this.algodClient
+        .simulateTransactions(request)
+        .do();
+      return response;
+    } catch (error) {
+      // console.error('Error in createAndSimulateTxn:', error);
+      throw error; // Re-throw the error after logging it
     }
+  }
 
-    handleSimulationResponse(response, abiMethod) {
-        try {
-            // Handle the simulation results
-            if (response.txnGroups[0].failureMessage) {
-                throw (response.txnGroups[0].failureMessage);
-            }
-            const rlog = response.txnGroups[0].txnResults[1].txnResult.logs.pop();
-            const rlog_ui = Uint8Array.from(Buffer.from(rlog, 'base64'));
-            const res_ui = rlog_ui.slice(4);
+  handleSimulationResponse(response, abiMethod) {
+    return this.simulationResultHandler(response, abiMethod);
+  }
 
-            // Decode the response based on the methodSpec
-            //HACK: Hacking this because the decode function doesn't work on bytes
-            let result;
-            if (abiMethod.returns.type.childType == 'byte') {
-                result = new TextDecoder().decode(res_ui);
-            } else {
-                result = abiMethod.returns.type.decode(res_ui);
-            }
+  decodeSimulationResponse(response, abiMethod) {
+    try {
+      // Handle the simulation results
+      if (response.txnGroups[0].failureMessage) {
+        throw response.txnGroups[0].failureMessage;
+      }
+      const rlog = response.txnGroups[0].txnResults[1].txnResult.logs.pop();
+      const rlog_ui = Uint8Array.from(Buffer.from(rlog, "base64"));
+      const res_ui = rlog_ui.slice(4);
 
-            return result;
-        } catch (error) {
-            // console.error('Error in handleSimulationResponse:', error);
-            throw error;  // Re-throw the error after logging it
-        }
+      // Decode the response based on the methodSpec
+      //HACK: Hacking this because the decode function doesn't work on bytes
+      let result;
+      if (abiMethod.returns.type.childType == "byte") {
+        result = new TextDecoder().decode(res_ui);
+      } else {
+        result = abiMethod.returns.type.decode(res_ui);
+      }
+
+      return result;
+    } catch (error) {
+      // console.error('Error in handleSimulationResponse:', error);
+      throw error; // Re-throw the error after logging it
     }
-
+  }
 }
 
 module.exports = CONTRACT;
--- index.js
+++ index.js
@@ -1,71 +1,311 @@
-require('dotenv').config();
-const algosdk = require('algosdk');
+require("dotenv").config();
+const algosdk = require("algosdk");
 
-const CONTRACT = require('./contract.js');
+const CONTRACT = require("./contract.js");
 
-const algodToken = '';  // Your Algod API token
-const algodServer = process.env.ALGOD_URL;  // Address of your Algod node
-const algodPort = '';  // Port of your Algod node
+const algodToken = ""; // Your Algod API token
+const algodServer = process.env.ALGOD_URL; // Address of your Algod node
+const algodPort = ""; // Port of your Algod node
 
 const algodClient = new algosdk.Algodv2(algodToken, algodServer, algodPort);
 
 // Load ARC200 specification
-const ARC200Spec = require('./ARC200.json');
+const ARC200Spec = require("./ARC200.json");
 
 const contractsData = [
-    { contractId: 6726425, contractInstanceName: "IRLToken" },
-    { contractId: 3905802, contractInstanceName: "VRC200" }
+  { contractId: 6726425, contractInstanceName: "IRLToken" },
+  { contractId: 3905802, contractInstanceName: "VRC200" },
 ];
 
+const BalanceBoxCost = 28500;
+const BalanceBoxSize = 33; // [0, ...addrTo.pk]
+const AllowanceBoxCost = 28100;
+const AllowanceBoxSize = 32; // sha256([1,...addrFrom.pk, ...addrTo.pk])
+
 (async () => {
-    for (const contractData of contractsData) {
+  for (const contractData of contractsData) {
+    try {
+      const newContractInstance = () =>
+        new CONTRACT(
+          contractData.contractId,
+          algodClient,
+          ARC200Spec,
+          process.env.WALLET_MNEMONIC
+        );
+      const contractInstance = newContractInstance();
+
+      const arc200_name = async () => {
+        const name = await contractInstance.arc200_name();
+        console.log(`Name: ${name}`);
+        return name;
+      };
+
+      const arc200_symbol = async () => {
+        const symbol = await contractInstance.arc200_symbol();
+        console.log(`Symbol: ${symbol}`);
+        return symbol;
+      };
+
+      const arc200_totalSupply = async () => {
+        const totalSupply = await contractInstance.arc200_totalSupply();
+        console.log(`Total Supply: ${totalSupply}`);
+        return totalSupply;
+      };
+
+      const arc200_decimals = async () => {
+        const decimals = await contractInstance.arc200_decimals();
+        console.log(`Number of Decimals: ${decimals}`);
+        return decimals;
+      };
+
+      const arc200_balanceOf = async (addr) => {
+        const balance = await contractInstance.arc200_balanceOf(addr);
+        console.log(`BalanceOf`)
+        console.log(`    ${addr}: ${balance}`);
+        return balance;
+      };
+
+      const arc200_allowance = async (addrSpender, addrFrom) => {
+        const allowance = await contractInstance.arc200_allowance(
+          addrSpender,
+          addrFrom
+        );
+        console.log(`Allowance`);
+        console.log(`    from: ${addrFrom}`);
+        console.log(`    to: ${addrSpender}`);
+        console.log(`    total: ${allowance}`);
+
+        return allowance;
+      };
+
+      const safe_arc200_transfer = async (addrTo, amt) => {
         try {
-            const contractInstance = new CONTRACT(contractData.contractId, algodClient, ARC200Spec, process.env.WALLET_MNEMONIC);
-
-            const wallet1 = 'C5NZ5SNL5EMOEVKFW3DS3DBG3FNMIYJAJY3U4I5SRCOXHGY33ML3TGHD24';
-            const wallet2 = 'OOEDQF6YL44JOIFBDXWVNREBXQ4IL53JMTA32R66S7GLKEP5WC4CL4SFLE';
-            const wallet3 = 'BUD2763FMK6EYVKGHWWUN4QKHPSPCVFUEPPI4PQCPGYVPGQ6GNKBX6IXCQ';
-            const { addr: senderAddress } = algosdk.mnemonicToSecretKey(process.env.WALLET_MNEMONIC);
-
-            const transferAmount = 10;
-
-            console.log(`Processing ${contractData.contractInstanceName}:`);
-
-            console.log(`Sender Address: ${senderAddress}`)
-
-            const name = await contractInstance.arc200_name();
-            console.log(`Name: ${name}`);
-
-            const symbol = await contractInstance.arc200_symbol();
-            console.log(`Symbol: ${symbol}`);
-
-            const totalSupply = await contractInstance.arc200_totalSupply();
-            console.log(`Total Supply: ${totalSupply}`);
-
-            const decimals = await contractInstance.arc200_decimals();
-            console.log(`Number of Decimals: ${decimals}`);
-
-            const balance = await contractInstance.arc200_balanceOf(senderAddress);
-            console.log(`Balance of ${senderAddress}: ${balance}`);
-
-            const allowance = await contractInstance.arc200_allowance(wallet1, wallet2);
-            console.log(`Allowance from: ${wallet1} to: ${wallet2} total: ${allowance}`);
-
-            console.log(`Transfer to: ${wallet3} amount: ${transferAmount}`);
-            const transfer = await contractInstance.arc200_transfer(wallet3, transferAmount);
-            console.log(transfer);
-
-            //TODO: Errors Out
-            console.log(`Approve: ${wallet1} Amount: ${transferAmount}`)
-            const approve = await contractInstance.arc200_approve(wallet1, transferAmount);
-            console.log(approve);
-
-            console.log(`Transfer from: ${wallet1} Transfer to: ${wallet2} amount: ${transferAmount}`);
-            const transferFrom = await contractInstance.arc200_transferFrom(wallet1, wallet2, transferAmount);
-            console.log(transferFrom);
-
-        } catch (error) {
-            console.error(`Error processing ${contractData.contractInstanceName}:`, error);
+          const contractInstance = newContractInstance();
+          const senderAddress = contractInstance.getSenderAddress();
+          console.log(`Transfer`);
+          console.log(`    to: ${addrTo}`);
+          console.log(`    amount: ${transferAmount}`);
+          // -------------------------------------
+          // preflight
+          //  check balanceOf senderAddress >= amt
+          //  check balanceOf addrTo >= 0
+          // -------------------------------------
+          const balanceOfSender = await contractInstance.arc200_balanceOf(
+            senderAddress
+          );
+          console.log(`    Balance of ${senderAddress}: ${balanceOfSender}`);
+          if (balanceOfSender < amt) {
+            console.log(
+              "    Expect failure due to insufficient funds to transfer"
+            );
+          }
+          const balanceOfAddrTo = await contractInstance.arc200_balanceOf(
+            addrTo
+          );
+          console.log(`    Balance of ${addrTo}: ${balanceOfAddrTo}`);
+          if (balanceOfAddrTo <= 0 && this.paymentAmount === 0) {
+            console.log(
+              "    Expect possible failure due to insufficient funds to pay for balance box"
+            );
+          }
+          // -------------------------------------
+          const response = await contractInstance
+            .arc200_transfer(addrTo, amt)
+            .catch(console.err);
+          if (!response?.response) {
+            console.log(`    Result: ${response}`);
+            return response;
+          } else {
+            console.log(
+              `    Retry with balance box payment (${BalanceBoxCost})`
+            );
+            contractInstance.setPaymentAmount(BalanceBoxCost);
+            const response = await contractInstance
+              .arc200_transfer(addrTo, amt)
+              .catch(console.err);
+            if (!response?.response) {
+              console.log(`    Result: ${response}`);
+              return response;
+            } else {
+              console.log("    Failure");
+              console.log(`    Result: 0`);
+              return 0;
+            }
+          }
+        } catch (e) {
+          console.log(e);
         }
+      };
+
+      const safe_arc200_transferFrom = async (addrFrom, addrTo, amt) => {
+        try {
+          const contractInstance = newContractInstance();
+          const senderAddress = contractInstance.getSenderAddress();
+          console.log(`TransferFrom`);
+          console.log(`    from: ${addrFrom}`);
+          console.log(`    to: ${addrTo}`);
+          console.log(`    amount: ${transferAmount}`);
+          // -------------------------------------
+          // preflight
+          //  check balanceOf addrFrom >= amt
+          //  check balanceOf addrTo >= 0
+          //  check allowance(senderAddress,addrFrom) >= amt
+          // -------------------------------------
+          const balanceOfAddrFrom = await contractInstance.arc200_balanceOf(
+            addrFrom
+          );
+          console.log(`    Balance of ${addrFrom}: ${balanceOfAddrFrom}`);
+          if (balanceOfAddrFrom < amt) {
+            console.log(
+              "    Expect failure due to insufficient funds to transfer"
+            );
+          }
+          const balanceOfAddrTo = await contractInstance.arc200_balanceOf(
+            addrTo
+          );
+          console.log(`    Balance of ${addrTo}: ${balanceOfAddrTo}`);
+          if (balanceOfAddrTo <= 0 && this.paymentAmount === 0) {
+            console.log(
+              "    Expect possible failure due to insufficient funds to pay for balance box"
+            );
+          }
+          const allowance = await contractInstance.arc200_allowance(
+            addrFrom,
+            senderAddress
+          );
+          console.log(
+            `    Allowance from ${addrFrom} to ${senderAddress}: ${allowance}`
+          );
+          if (allowance < amt) {
+            console.log(
+              "    Expect failure due to insufficient allowance to transfer"
+            );
+          }
+          // -------------------------------------
+          const response = await contractInstance
+            .arc200_transferFrom(addrFrom, addrTo, amt)
+            .catch(console.err);
+          if (!response?.response) {
+            console.log(`    Result: ${response}`);
+            return response;
+          } else {
+            console.log(
+              `    Retry with balance box payment (${BalanceBoxCost})`
+            );
+            contractInstance.setPaymentAmount(BalanceBoxCost);
+            const response = await contractInstance
+              .arc200_transferFrom(addrFrom, addrTo, amt)
+              .catch(console.err);
+            if (!response?.response) {
+              console.log(`    Result: ${response}`);
+              return response;
+            } else {
+              console.log("    Failure");
+              console.log(`    Result: 0`);
+              return 0;
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+
+      const safe_arc200_approve = async (addrSpender, amt) => {
+        try {
+          const contractInstance = newContractInstance();
+          console.log(`Approve: ${addrSpender} Amount: ${amt}`);
+          // -------------------------------------
+          const response = await contractInstance
+            .arc200_approve(addrSpender, amt)
+            .catch(console.err);
+          if (!response?.response) {
+            console.log(`    Result: ${response}`);
+            return response;
+          } else {
+            console.log(
+              `    Retry with balance box payment (${AllowanceBoxCost})`
+            );
+            contractInstance.setPaymentAmount(AllowanceBoxCost);
+            const response = await contractInstance
+              .arc200_approve(addrSpender, amt)
+              .catch(console.err);
+            if (!response?.response) {
+              console.log(`    Result: ${response}`);
+              return response;
+            } else {
+              console.log("    Failure");
+              console.log(`    Result: 0`);
+              return 0;
+            }
+          }
+        } catch (e) {
+          console.log(e);
+        }
+      };
+
+      const wallet1 =
+        "C5NZ5SNL5EMOEVKFW3DS3DBG3FNMIYJAJY3U4I5SRCOXHGY33ML3TGHD24";
+      const wallet2 =
+        "OOEDQF6YL44JOIFBDXWVNREBXQ4IL53JMTA32R66S7GLKEP5WC4CL4SFLE";
+      const wallet3 =
+        "BUD2763FMK6EYVKGHWWUN4QKHPSPCVFUEPPI4PQCPGYVPGQ6GNKBX6IXCQ";
+      const wallet4 =
+        "RTKWX3FTDNNIHMAWHK5SDPKH3VRPPW7OS5ZLWN6RFZODF7E22YOBK2OGPE";
+      const wallet5 =
+        "ET4L2UGCB67ASL2HRRXSYIH64ELUJBRRGGIF5D7FI46NLHYX7OOG3KVZWM";
+
+      const { addr: senderAddress } = algosdk.mnemonicToSecretKey(
+        process.env.WALLET_MNEMONIC
+      );
+
+      const transferAmount = 10;
+
+      console.log(`Processing ${contractData.contractInstanceName}:`);
+
+      console.log(`Sender Address: ${senderAddress}`);
+
+      // TOKEN INFO
+
+      await arc200_name();
+      await arc200_symbol();
+      await arc200_totalSupply();
+      await arc200_decimals();
+
+      // BALANCES
+
+      await arc200_balanceOf(senderAddress);
+      await arc200_balanceOf(wallet1);
+      await arc200_balanceOf(wallet2);
+      await arc200_balanceOf(wallet3);
+      await arc200_balanceOf(wallet4);
+      await arc200_balanceOf(wallet5);
+
+      // ALLOWANCES
+
+      await arc200_allowance(wallet1, wallet2);
+      await arc200_allowance(wallet4, wallet1);
+      await arc200_allowance(wallet1, senderAddress);
+      await arc200_allowance(wallet4, senderAddress);
+
+      // TRANSFER
+
+      await safe_arc200_transfer(wallet1, transferAmount);
+      await safe_arc200_transfer(wallet5, transferAmount);
+
+      // APPROVE
+
+      await safe_arc200_approve(wallet1, transferAmount);
+
+      // TRANSFER FROM
+
+      await safe_arc200_transferFrom(wallet1, wallet2, transferAmount);
+      await safe_arc200_transferFrom(wallet4, wallet1, transferAmount);
+      await safe_arc200_transferFrom(wallet4, wallet5, transferAmount);
+    } catch (error) {
+      console.error(
+        `Error processing ${contractData.contractInstanceName}:`,
+        error
+      );
     }
+  }
 })();
